fbd0e836d1edb35854acc56d9470d252c06baa12
shihd1 <shihd@mca.org.tw>
2024-11-07 22:21:08 -0500

    Fix bugs

--- etovucca/cgi-bin/register.cgi
+++ etovucca/cgi-bin/register.cgi
@@ -1,5 +1,4 @@
 #!/bin/bash
-PATH_TO_MACHINE=./etovucca
 
 render_register() {
     echo "Content-Type: text/html"
@@ -22,31 +21,4 @@ render_register() {
     echo '<a href="./home.cgi">Return to Homepage</a><br>'
 }
 
-register_voter() {
-    id=`$PATH_TO_MACHINE add-voter "${array[name]}" ${array[county]} ${array[zipc]} ${array[dob]}`
-    name_check=`./name_helper.py "${array[name]}"`
-    if [ $id -eq 0 ]; then
-        echo "<div>Error in registering voter. Please try again.</div>"
-    elif [ ! -n $name_check ]; then
-        echo "<div>Error in registering voter, invalid name. Please try again.</div>"
-    else
-        echo "$name_check Your ID is: $id</div>"
-    fi
-}
-
 render_register 
-
-if [ ! -z $QUERY_STRING ]; then
-    # Parsing code from https://stackoverflow.com/a/3919908
-    saveIFS=$IFS
-    IFS='=&'
-    parm=($QUERY_STRING)
-    IFS=$saveIFS
-    declare -A array
-    for ((i=0; i<${#parm[@]}; i+=2))
-    do
-        array[${parm[i]}]=${parm[i+1]}
-    done
-
-    register_voter
-fi
--- etovucca/cgi-bin/save_file.py
+++ etovucca/cgi-bin/save_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import cgi
 import os
+import subprocess
 from urllib.parse import unquote
 
 UPLOAD_DIR = "./uploads/"
+PATH_TO_MACHINE = "./etovucca"
 
 def save_file():
     print("Content-Type: text/html")
@@ -15,17 +17,43 @@ def save_file():
     zipc = form.getvalue("zipc")
     dob = form.getvalue("dob")
 
+    # First save the photo if provided
     file_item = form["photo"]
-
     if file_item.filename:
-        with open(UPLOAD_DIR+'/'+unquote(file_item.filename), "wb") as f:
+        with open(UPLOAD_DIR + '/' + unquote(file_item.filename), "wb") as f:
             f.write(file_item.file.read())
-
-        print(f"<b>Voter registered successfully with photo uploaded</b><br>")
+        photo_status = "Photo uploaded successfully."
     else:
-        print("<b>Voter registered, but no photo was uploaded.</b><br>")
+        photo_status = "No photo was uploaded."
 
-    print('<a href="./home.cgi">Return to Homepage</a>')
+    # Now register the voter
+    try:
+        # Call name helper to validate the name
+        name_check = subprocess.run(['./name_helper.py', name], 
+                                  capture_output=True, 
+                                  text=True)
+        
+        if name_check.returncode != 0 or not name_check.stdout.strip():
+            print("<div>Error in registering voter, invalid name. Please try again.</div>")
+            return
+
+        # Register voter with etovucca machine
+        result = subprocess.run([PATH_TO_MACHINE, 'add-voter', name, county, zipc, dob],
+                              capture_output=True,
+                              text=True)
+        
+        voter_id = result.stdout.strip()
+        
+        if voter_id and voter_id != "0":
+            print(f"<div>{name_check.stdout} Your ID is: {voter_id}</div>")
+            print(f"<div>{photo_status}</div>")
+        else:
+            print("<div>Error in registering voter. Please try again.</div>")
+            
+    except Exception as e:
+        print(f"<div>Error during registration: {str(e)}</div>")
+
+    print('<br><a href="./home.cgi">Return to Homepage</a>')
 
 if __name__ == "__main__":
     save_file()
